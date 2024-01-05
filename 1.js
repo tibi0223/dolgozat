6baf995334e927f38ce264762468baf3f0731f4d
Kaplonyi Tibor <kaplonyi.tibor2000@gmail.com>
2024-01-05 19:32:18 +0100

    first commit

---
+++ 1.js
@@ -0,0 +1,19 @@
+function szepTomb(arr) {
+    if (!Array.isArray(arr)) {
+      return false;
+    }
+  
+    const firstType = typeof arr[0];
+  
+    for (let i = 1; i < arr.length; i++) {
+      if (typeof arr[i] !== firstType) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  // Példák
+  console.log(szepTomb(['alma', 'korte', 'szilva', 'barack']));
+  console.log(szepTomb([10, 20, 30, 40, '50', 60, 70]));
+  
